6d46d788cc51398a035a5aef4b44a0fd632cc626
Tamakuwala <S0349821@stanfordhealthcare.org>
2022-11-29 15:25:14 -0800

    added static folder

--- KeywordExtractor-Datasets-master/retervie_data.py
+++ KeywordExtractor-Datasets-master/retervie_data.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 import os 
 from alive_progress import alive_bar
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
 
 
 def fetch_all_data():
@@ -18,8 +21,17 @@ def fetch_all_data():
         for document in cursor:
             data_list.append([document["document_data"],document["document_key"]])
             bar()
+    mongoclinet.close()
     return data_list
 
+
+# dimension of A = (a*b) and b = (c*b)
+def cosine_similarity(A,B):
+    num=np.dot(A,B.T)
+    p1=np.sqrt(np.sum(A**2,axis=1))[:,np.newaxis]
+    p2=np.sqrt(np.sum(B**2,axis=1))[np.newaxis,:]
+    return num/(p1*p2)
 if __name__ == "__main__":
-    data=fetch_all_data()
-    print(data[0])
+    #data=fetch_all_data()
+    #print(data[0])
+    print(cosine_similarity([1,1,1],[1,1,1]))
